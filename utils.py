d67ec83ab28261323a86fd91e8cd7c0bd20a3422
sara-acar <sara.betul@icloud.com>
2026-05-13 10:58:39 +0200

    essai

--- utils.py
+++ utils.py
@@ -1,7 +1,6 @@
 def parsePDB(file_name):
-    RNA = "RNA found in" + file_name 
-    return RNA
+    RNA = "RNA found in " + file_name 
+    return RNA                        
 
 def generate_dot_bracket(model):
-    print("dot-bracket notation of the " + model)
-     
+    print("dot-bracket notation of the " + model) 
